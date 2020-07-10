b7395eb3619228e014384a6091903ecbc7a92d42
Bonekyaw <phonenai2014@gmail.com>
2020-07-10 23:51:14 +0630

    Admin panel setup

--- resources/js/router.js
+++ resources/js/router.js
@@ -5,8 +5,24 @@ Vue.use(Router);
 
 import firstPage from './component/pages/myFirstPage.vue';
 import secondPage from './component/pages/secondPage.vue';
+//Project Page
+import home from './component/pages/home';
+import tags from './component/pages/tags';
 
 const routes = [
+    //Project Route
+    {
+        path: '/',
+        component: home
+    },
+    {
+        path: '/tags',
+        component: tags
+    },
+
+
+
+
     {
         path: '/first',
         component: firstPage
